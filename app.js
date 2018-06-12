7bbacec6edb662450517d3eae233896d05117adf
László Takács <Laszlo.Takacs@barion.com>
2018-06-12 14:37:15 +0200

    fix redirect

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var callbackRouter = require('./routes/callback');
 var networkLogRouter = require('./routes/networklog');
 var paymentRouter = require('./routes/payment');
+var redirectRouter = require('./routes/redirect');
 
 var app = express();
 // init scripts:
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/network', networkLogRouter);
 app.use('/callback', callbackRouter);
 app.use('/payment', paymentRouter);
+app.use('/redirect', redirectRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/redirect.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var path = require("path");
+var ejs = require('ejs');
+var async = require('async');
+var db = require('../utils/database');
+
+
+router.get('/', function(req, res, next) {
+    //res.render('index', { title: 'Express' });
+    //res.sendFile(path.join(__dirname + '/../public/views/index.html'));
+    res.render('redirect');
+  });
+
+/*router.get('/', function(req, res, next){
+    var dbResult;
+    async.series([
+        function (callback) {
+            db.getNetworkData(function (err, result) {
+                if (err) {
+                    dbResult = { status: "FAILED" }
+                    return callback(dbResult)
+                }
+                dbResult = result
+                callback()
+            })
+        }], function (err) {
+            res.setHeader('Content-Type', 'text/html')
+            if (!err) {
+                res.status(200);
+                var rows = [];
+                dbResult.forEach(item => {
+                    var row = {
+                        happenedAt : item.happenedAt,
+                        type : item.type,
+                        body : item.body
+                    };
+                    rows.push(row);
+                });
+                res.render('callbacks', { callbackRows: rows });
+            } else {
+                res.status(500);
+                res.end;
+            }
+        })
+})*/
+
+module.exports = router;
