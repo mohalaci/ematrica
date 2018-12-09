2f4bf69d056cda985755ca56a7a7e52f7e0f42f3
Laszlo Takacs <laszlo.takacs@barion.com>
2018-12-09 19:20:13 +0100

    fix redirect page

--- public/javascripts/ematrica-redirect.js
+++ public/javascripts/ematrica-redirect.js
@@ -42,7 +42,7 @@ var app = new Framework7({
 var $$ = Dom7;
 var mainView = app.views.create('.view-main');
 
-app.statusbar.setIosTextColor('white');
+app.statusbar.setTextColor('white');
 app.statusbar.setBackgroundColor('#0097DB');
 
 app.on('init', function() {
